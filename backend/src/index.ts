4e15e295590d8fde57628d6b3ce47d8b1d645cb8
Guilherme Gambeti <ggambeti@atlassian.com>
2020-10-11 17:09:37 +1100

    Removed build folders

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,13 +2,14 @@ import { Request, Response } from 'express';
 import express from 'express';
 import path from 'path';
 
-const isDev = process.env.NODE_ENV !== 'production';
+const isProduction = process.env.NODE_ENV === 'production';
 const PORT = process.env.PORT || 5000;
+const frontendPath = isProduction ? '../frontend/build' : '../../frontend/build';
 
 const app = express();
 
 // Priority serve any static files.
-app.use(express.static(path.resolve(__dirname, '../../frontend/build')));
+app.use(express.static(path.resolve(__dirname, frontendPath)));
 
 // Answer API requests.
 app.get('/api', (req: Request, res: Response) => {
@@ -18,9 +19,9 @@ app.get('/api', (req: Request, res: Response) => {
 
 // All remaining requests return the React app, so it can handle routing.
 app.get('*', (request: Request, response: Response) => {
-    response.sendFile(path.resolve(__dirname, '../../frontend/build', 'index.html'));
+    response.sendFile(path.resolve(__dirname, frontendPath, 'index.html'));
 });
 
 app.listen(PORT,  () => {
-    console.error(`Node ${isDev ? 'dev server' : 'cluster worker '+process.pid}: listening on port ${PORT}`);
+    console.log(`Node ${isProduction ? 'server' : 'dev server'} listening on port ${PORT}`);
 });
